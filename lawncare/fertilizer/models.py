e4c7c72653d3f67cbb4cee17904960561c1a64f4
Mark Landry <mark_landry2@yahoo.com>
2023-09-11 15:13:56 -0400

    create models Fertilizer and Application

--- lawncare/fertilizer/models.py
+++ lawncare/fertilizer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Fertilizer(models.Model):
+    name = models.CharField(max_length=200)
+    bag_weight = models.IntegerField(default=0)
+    bag_coverage =  models.IntegerField(default=0)
+    percent_N = models.DecimalField(max_digits=4, decimal_places=2)
+    percent_P = models.DecimalField(max_digits=4, decimal_places=2)
+    percent_X = models.DecimalField(max_digits=4, decimal_places=2)
+
+class Application(models.Model):
+    fertilizer = models.ForeignKey(Fertilizer, on_delete=models.CASCADE) 
+    bags_applied = models.DecimalField(max_digits=4, decimal_places=2)
+    date_applied = models.DateField()
